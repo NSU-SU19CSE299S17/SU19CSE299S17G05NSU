ee0f2e4783bffbfa3ccb328916b8c8cdf43e6d5c
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-07-15 02:46:06 +0600

    Issue #6 : Sign-In Function

--- Project code/app/src/main/java/com/example/chat_app/RegActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/RegActivity.java
@@ -3,6 +3,7 @@ package com.example.chat_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -23,12 +25,15 @@ public class RegActivity extends AppCompatActivity {
     private EditText mPhoneNumber, mCode ;
     private Button mSend ;
     String mVerificationId ;
+
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
         FirebaseApp.initializeApp(this);
+
+        userIsLoggedIn();
 
         mPhoneNumber = findViewById(R.id.phoneNumber) ;
         mCode = findViewById(R.id.code) ;
@@ -55,7 +60,25 @@ public class RegActivity extends AppCompatActivity {
         };
 
     }
-    
+        private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential){
+            FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                        userIsLoggedIn();
+                }
+            });
+    }
+
+    private void userIsLoggedIn(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+            return;
+        }
+    }
+
     private void startPhoneNumberVerification() {
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
                 mPhoneNumber.getText().toString(),
