669ec6d950693a7664812bbc052e2a0b519d2f13
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-07-15 02:29:29 +0600

    Issue #6 : Handling callbacks

--- Project code/app/src/main/java/com/example/chat_app/RegActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/RegActivity.java
@@ -1,5 +1,6 @@
 package com.example.chat_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,7 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
@@ -15,6 +22,7 @@ import java.util.concurrent.TimeUnit;
 public class RegActivity extends AppCompatActivity {
     private EditText mPhoneNumber, mCode ;
     private Button mSend ;
+    String mVerificationId ;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +36,32 @@ public class RegActivity extends AppCompatActivity {
 
         mSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startPhoneNumberVerification();
+            public void onClick(View v) {
+                if(mVerificationId != null)
+                    verifyPhoneNumberWithCode();
+                else
+                    startPhoneNumberVerification();
             }
         });
-    }
 
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {}
+        };
+
+    }
+    
     private void startPhoneNumberVerification() {
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
                 mPhoneNumber.getText().toString(),
                 60,
                 TimeUnit.SECONDS,
-                this, mCallbacks;
-        );
+                this, mCallbacks);
+
     }
 }
