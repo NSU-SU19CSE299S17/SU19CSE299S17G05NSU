18f68c93353afaa5cf2c34eecb64c67608160048
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-07-15 01:40:48 +0600

    Issue #6 : Phone Number Verification added

--- Project code/app/src/main/java/com/example/chat_app/RegActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/RegActivity.java
@@ -3,14 +3,19 @@ package com.example.chat_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class RegActivity extends AppCompatActivity {
     private EditText mPhoneNumber, mCode ;
     private Button mSend ;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,21 @@ public class RegActivity extends AppCompatActivity {
         mPhoneNumber = findViewById(R.id.phoneNumber) ;
         mCode = findViewById(R.id.code) ;
         mSend = findViewById(R.id.verify);
+
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPhoneNumberVerification();
+            }
+        });
+    }
+
+    private void startPhoneNumberVerification() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this, mCallbacks;
+        );
     }
 }
