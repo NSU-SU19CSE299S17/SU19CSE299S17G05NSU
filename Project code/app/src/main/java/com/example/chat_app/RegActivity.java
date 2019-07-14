3fc88da43950599853cb311ca45d38be8d07d244
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-07-15 01:23:48 +0600

    Issue #6 : Initialized variables to the activity.xml
    Few variables are initialized to make the buttons functional

--- Project code/app/src/main/java/com/example/chat_app/RegActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/RegActivity.java
@@ -3,12 +3,22 @@ package com.example.chat_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
 
 public class RegActivity extends AppCompatActivity {
-
+    private EditText mPhoneNumber, mCode ;
+    private Button mSend ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+        FirebaseApp.initializeApp(this);
+
+        mPhoneNumber = findViewById(R.id.phoneNumber) ;
+        mCode = findViewById(R.id.code) ;
+        mSend = findViewById(R.id.verify);
     }
 }
