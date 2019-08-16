48225bc88c25de4c972be43cd48b645aa1817f21
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-16 16:50:05 +0600

    Issue #2 : Fixed compatibility issues
    Due to some compatibilty settings, the app was not running on the emulator. Fixed that.

--- Project code/app/src/main/java/com/example/chat_app/MainActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/MainActivity.java
@@ -1,26 +1,24 @@
 package com.example.chat_app;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity
+{
     private Toolbar mToolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("ChitChat");
-
 
     }
 
