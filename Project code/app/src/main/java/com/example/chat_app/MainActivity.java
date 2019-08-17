9ade3309a99ea7ad699c3a701c1ee86a742ee8dd
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-17 21:34:37 +0600

    Feature #7 : Added pager to mainActivity

--- Project code/app/src/main/java/com/example/chat_app/MainActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/MainActivity.java
@@ -4,12 +4,16 @@ package com.example.chat_app;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity
 {
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
