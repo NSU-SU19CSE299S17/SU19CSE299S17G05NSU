58acb3cba9a01344d47ebbc245e580dea3bf2fd2
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-18 22:08:33 +0600

    Feature #19 : Initialized fields
    A method is created in which the fields in the login page are defined.

--- Project code/app/src/main/java/com/example/chat_app/LoginActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/LoginActivity.java
@@ -4,19 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
-
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail, UserPassword;
+    private TextView NeedNewAccountLink, ForgotPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+    }
+
+    private void InitializeFields() {
+
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.new_account_link);
+        ForgotPasswordLink = (TextView) findViewById(R.id.forget_password_link);
     }
 
     @Override
