fb1a9a9e4e73a038a1c46aabfc324a677711432d
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-19 00:46:48 +0600

    Feature #19 : Method for creating an accout
    Method for creating an account using firebase authentication system is defined.

--- Project code/app/src/main/java/com/example/chat_app/RegisterActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/RegisterActivity.java
@@ -1,13 +1,21 @@
 package com.example.chat_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,11 +24,14 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText UserEmail, UserPassword ;
     private TextView AlreadyHaveAccountLink;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         InitializeFields();
 
@@ -30,7 +41,45 @@ public class RegisterActivity extends AppCompatActivity {
                 SendUserToLoginActivity();
             }
         });
+        
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateNewAccount();
+            }
+        });
     }
+
+
+    private void CreateNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter email address", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Account Created", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                String message = task.getException().toString();
+                                Toast.makeText(RegisterActivity.this, "Error: " +message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
 
     private void InitializeFields() {
 
