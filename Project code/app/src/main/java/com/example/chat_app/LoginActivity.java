1ad2cb7776ab11e1d577fa980678806fe4634971
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-18 22:18:27 +0600

    Feature #19 : Updated the login activity
    A link for new account is defined.

--- Project code/app/src/main/java/com/example/chat_app/LoginActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +25,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         InitializeFields();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToRegisterActivity();
+            }
+        });
     }
 
     private void InitializeFields() {
@@ -47,5 +55,10 @@ public class LoginActivity extends AppCompatActivity {
     private void SendUserToMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent);
+    }
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
