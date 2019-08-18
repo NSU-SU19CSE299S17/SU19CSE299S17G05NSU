faad011b47c023906975c0a77306ce2728800813
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-19 02:05:26 +0600

    Feature #7 : Created Menu resource directory
    An drop-down option is added to the main activity for the user to sign out.

--- Project code/app/src/main/java/com/example/chat_app/MainActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.chat_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -53,5 +57,30 @@ public class MainActivity extends AppCompatActivity
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_option){
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if(item.getItemId() == R.id.main_find_friends_option){
+
+        }
+        if(item.getItemId() == R.id.main_settings_option){
+
+        }
+        return true;
     }
 }
