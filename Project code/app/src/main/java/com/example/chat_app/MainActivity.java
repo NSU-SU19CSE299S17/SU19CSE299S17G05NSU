c943c510679c99b75dc323f495498bf61c24ec4f
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-21 16:12:03 +0600

    Feature #21 : Created method for settings
    Users can change username and set active status

--- Project code/app/src/main/java/com/example/chat_app/MainActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/MainActivity.java
@@ -54,11 +54,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private void SendUserToLoginActivity() {
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(loginIntent);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -79,8 +74,19 @@ public class MainActivity extends AppCompatActivity
 
         }
         if(item.getItemId() == R.id.main_settings_option){
-
+            SendUserToSettingsActivity();
         }
         return true;
     }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void SendUserToSettingsActivity() {
+        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(settingsIntent);
+    }
+
 }
