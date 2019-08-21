60d21ce8e38244aadb69dde38d61ffc407ce15a6
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-22 02:01:04 +0600

    Feature #21 : Method for retrieving previous data

--- Project code/app/src/main/java/com/example/chat_app/SettingsActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/SettingsActivity.java
@@ -14,8 +14,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -48,7 +51,10 @@ public class SettingsActivity extends AppCompatActivity {
                 UpdateSettings();
             }
         });
+
+        RetrieveUserInfo();
     }
+
 
 
     private void InitializeFields() {
@@ -94,6 +100,41 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
+    private void RetrieveUserInfo() {
+        RootRef.child("Users").child(currentUserId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if((dataSnapshot.exists()) && (dataSnapshot.hasChild("name") && (dataSnapshot.hasChild("image")))){
+                            String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                            String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+                            String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            userStatus.setText(retrieveStatus);
+
+                        }
+
+                        else if((dataSnapshot.exists()) && (dataSnapshot.hasChild("name"))){
+
+                            String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                            String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            userStatus.setText(retrieveStatus);
+                        }
+
+                        else{
+                            Toast.makeText(SettingsActivity.this, "Please set and update your profile information", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
 
     private void SendUserToMainActivity() {
         Intent mainIntent = new Intent(SettingsActivity.this, MainActivity.class);
