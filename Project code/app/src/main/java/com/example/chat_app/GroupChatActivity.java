407aec3b0e803fb36255b3e89a620d43e6e6ec6f
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-24 01:01:20 +0600

    Feature #23 : Method created to redirect from groupName to chat

--- Project code/app/src/main/java/com/example/chat_app/GroupChatActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/GroupChatActivity.java
@@ -5,6 +5,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -18,11 +19,16 @@ public class GroupChatActivity extends AppCompatActivity {
     private ScrollView mScrollView;
     private TextView displayTextMessages;
 
+    private String currentGroupName ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
+
+        currentGroupName = getIntent().getExtras().get("groupName").toString();
+        Toast.makeText(GroupChatActivity.this, currentGroupName, Toast.LENGTH_SHORT).show();
         
         InitializeFields();
     }
@@ -31,7 +37,7 @@ public class GroupChatActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar)findViewById(R.id.group_chat_bar_layout);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Group Name");
+        getSupportActionBar().setTitle(currentGroupName);
 
         sendMessageButton = (ImageButton)findViewById(R.id.send_message_button);
         userMessageInput = (EditText)findViewById(R.id.input_group_message);
