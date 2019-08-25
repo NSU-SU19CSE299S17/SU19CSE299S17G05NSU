c718548506d9b6f9ba4ef05ebc75a16b1c8d16cf
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2019-08-26 02:02:47 +0600

    Feature #28 : Updated profile Activty

--- Project code/app/src/main/java/com/example/chat_app/ProfileActivity.java
+++ Project code/app/src/main/java/com/example/chat_app/ProfileActivity.java
@@ -1,21 +1,75 @@
 package com.example.chat_app;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private String receiveUserId;
+    private CircleImageView userProfileImage ;
+    private TextView userProfileStatus, userProfileName ;
+    private Button SendMessageRequestButton;
 
+    private DatabaseReference UserRef ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
         receiveUserId = getIntent().getExtras().get("visit_user_id").toString() ;
 
-        Toast.makeText(this, "User ID: " +receiveUserId, Toast.LENGTH_SHORT).show();
+
+        userProfileImage = (CircleImageView) findViewById(R.id.visit_profile_image);
+        userProfileName = (TextView) findViewById(R.id.visit_user_name);
+        userProfileStatus = (TextView) findViewById(R.id.visit_profile_status);
+        SendMessageRequestButton = (Button) findViewById(R.id.send_message_request_button);
+        
+        RetrieveUserInfo();
+    }
+
+    private void RetrieveUserInfo()
+    {
+        UserRef.child(receiveUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if ((dataSnapshot.exists())  &&  (dataSnapshot.hasChild("image")))
+                {
+                    String userImage = dataSnapshot.child("image").getValue().toString();
+                    String userName = dataSnapshot.child("name").getValue().toString();
+                    String userstatus = dataSnapshot.child("status").getValue().toString();
+
+                }
+                else
+                {
+                    String userName = dataSnapshot.child("name").getValue().toString();
+                    String userstatus = dataSnapshot.child("status").getValue().toString();
+
+                    userProfileName.setText(userName);
+                    userProfileStatus.setText(userstatus);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
     }
 }
